05e7b20a8a73ebcf35bdfccf2e99ef47c22c1814
ivokoby <ivokoby@gmail.com>
2022-11-23 14:48:00 -0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home () {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      ESTUDIO CACTUS VISUALIZER TEST
+      <p>Hola</p>
+    </div>
+  )
+}
